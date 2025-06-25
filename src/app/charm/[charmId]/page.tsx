110fb824082acd9da464746ecf6e048fd6cb40f0
nate <nathanclingman@aol.com>
2025-06-25 19:04:07 -0400

    more ai slop to get it working on vencel or whatever it's called

--- src/app/charm/[charmId]/page.tsx
+++ src/app/charm/[charmId]/page.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-type Props = {
+type PageProps = {
   params: {
     charmId: string;
   };
 };
 
-export default function CharmPage({ params }: Props) {
+export default async function CharmPage({ params }: PageProps) {
+  // TODO: Replace this with your actual fetch call
+  // const res = await fetch(`https://api.example.com/charm/${params.charmId}`);
+  // const data = await res.json();
+
   return (
-    <div>
+    <main>
       <h1>Charm ID: {params.charmId}</h1>
-    </div>
+    </main>
   );
 }
